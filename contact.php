702e92004ebd0f8f1b71b5ca9afd645f49c7f3b4
Naoto13 <yamaura777@gmail.com>
2019-05-15 23:46:15 +0900

    change

--- contact.php
+++ contact.php
@@ -11,7 +11,6 @@ Template Name: Contact ~お問い合わせ~
 		<?php get_template_part(' content ',' menu ');?>
 		
 		<div id="main">
-
 			<!-- Contact -->
 			<section id="contact" class="site-width">
 			<h1 class="title"><?php echo get_the_title(); ?></h1>
@@ -20,17 +19,14 @@ Template Name: Contact ~お問い合わせ~
 						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 							<?php the_content(); ?>
 						</div>
-						<?php endwhile; //繰り返し処理終了
+					<?php endwhile; //繰り返し処理終了
 					else :	//ここから記事が見つからなかった場合の処理 ?>
 						<div class="post">
 							<h2>記事はありません</h2>
 							<p>お探しの記事は見つかりませんでした</p>
 						</div>
 					<?php endif; //WPのループ終了?>
-
 			</section>
-
-
 		</div>
 <!-- フッター -->
 <?php get_footer(); ?>
